fdc6dfded6a7429fa35204dde8f9d15784804ca7
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2024-08-28 23:36:17 +0700

    first commit

---
+++ js/2.js
@@ -0,0 +1,7 @@
+document.getElementById('attendance-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+ 
+    // Handle form data here
+    alert('Absensi berhasil dikirim!');
+ });
+ 
