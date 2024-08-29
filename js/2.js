5c629bde0fa9a1c83fdef6905c5ee9b2f79efc3f
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2024-08-29 07:26:24 +0700

    first commit

--- js/2.js
+++ js/2.js
@@ -5,3 +5,16 @@ document.getElementById('attendance-form').addEventListener('submit', function(e
     alert('Absensi berhasil dikirim!');
  });
  
+ document.getElementById('open-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'flex';
+});
+
+document.getElementById('close-modal').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+});
+
+window.addEventListener('click', function(event) {
+    if (event.target === document.getElementById('modal')) {
+        document.getElementById('modal').style.display = 'none';
+    }
+});
