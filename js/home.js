e7f875ef9ac1ea77a88563a12705b5e1c983d26f
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2024-08-30 18:43:50 +0700

    first commit

--- js/home.js
+++ js/home.js
@@ -15,26 +15,40 @@ document.addEventListener("DOMContentLoaded", function() {
     });
  });
  
- // Modal Functionality
  document.addEventListener('DOMContentLoaded', function() {
     const openModalBtn = document.getElementById('open-modal');
     const closeModalBtn = document.getElementById('close-modal');
     const modal = document.getElementById('modal');
- 
+
+    // Fungsi untuk menampilkan modal
+    function showModal() {
+        modal.style.display = 'flex';
+        setTimeout(function() {
+            modal.classList.add('active');
+        }, 10); // Timeout untuk memastikan modal muncul dengan animasi
+    }
+
+    // Fungsi untuk menyembunyikan modal
+    function hideModal() {
+        modal.classList.add('hide');
+        setTimeout(function() {
+            modal.style.display = 'none';
+            modal.classList.remove('active', 'hide');
+        }, 500); // Durasi animasi fadeOutScale
+    }
+
     openModalBtn.addEventListener('click', function(e) {
         e.preventDefault();
-        modal.style.display = 'flex';
+        showModal();
     });
- 
+
     closeModalBtn.addEventListener('click', function() {
-        modal.style.display = 'none';
+        hideModal();
     });
- 
+
     window.addEventListener('click', function(event) {
         if (event.target == modal) {
-            modal.style.display = 'none';
+            hideModal();
         }
     });
- });
- 
- 
+});
