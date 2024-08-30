40ad72e6658c5f2d12d5fd264f554c4fbd07f7fc
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2024-08-30 07:48:57 +0700

    first commit

--- js/home.js
+++ js/home.js
@@ -1,3 +1,21 @@
+// Toggle Navbar Menu
+const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById('nav-menu');
+
+hamburger.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    hamburger.classList.toggle('active');
+});
+
+// Optional: Close Navbar when clicking outside
+window.addEventListener('click', function(event) {
+    if (!event.target.closest('.nav-menu') && !event.target.closest('.hamburger')) {
+        navMenu.classList.remove('show');
+        hamburger.classList.remove('active');
+    }
+});
+
+
 // Open Modal
 document.getElementById('open-modal').addEventListener('click', function(event) {
     event.preventDefault();
