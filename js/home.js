b0c1dcb2707b5f7797becccd801fdb5698c7b51e
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2024-08-30 11:23:29 +0700

    first commit

--- js/home.js
+++ js/home.js
@@ -15,7 +15,6 @@ window.addEventListener('click', function(event) {
     }
 });
 
-
 // Open Modal
 document.getElementById('open-modal').addEventListener('click', function(event) {
     event.preventDefault();
@@ -31,24 +30,5 @@ document.getElementById('close-modal').addEventListener('click', function() {
 window.addEventListener('click', function(event) {
     if (event.target === document.getElementById('modal')) {
         document.getElementById('modal').style.display = 'none';
-    }
-});
-
-// JavaScript untuk menampilkan modal
-const modal = document.querySelector('.modal');
-const openModalBtn = document.querySelector('.open-modal');
-const closeModalBtn = document.querySelector('.close');
-
-openModalBtn.addEventListener('click', () => {
-    modal.classList.add('active');
-});
-
-closeModalBtn.addEventListener('click', () => {
-    modal.classList.remove('active');
-});
-
-modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-modal.classList.remove('active');
     }
 });
